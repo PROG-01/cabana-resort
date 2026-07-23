ebe093784efb0ac9e86f921a6c784a4df5d4ed95
PROG-01 <protusga9@gmail.com>
2026-07-23 10:33:36 +0200

    Implement map rendering and booking API endpoint

--- public/script.js
+++ public/script.js
@@ -1,7 +1,46 @@
-fetch('/api/map')
-   .then(function (response){
+const mapContainer = document.querySelector("#map");
+
+const tileClasses = {
+    ".": "grass",
+    "#": "road",
+    "c": "cabin",
+    "W": "water",
+    "p": "pool"
+};
+
+fetch("/api/map")
+    .then(function(response){
         return response.json();
-   })
-   .then(function (data){
-        console.log(data);
-   });
+    })
+    .then(function(mapData){
+
+        mapData.forEach(function(row, rowIndex){
+
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("row");
+
+            row.split("").forEach(function(cell, colIndex){
+
+                const cellDiv = document.createElement("div");
+
+                cellDiv.classList.add("cell");
+                if(cell === "W"){
+                    cellDiv.style.cursor = "pointer";
+                    
+                    cellDiv.addEventListener("click", function(){
+                         console.log(rowIndex, colIndex);
+                    })
+
+                }
+
+                cellDiv.classList.add(tileClasses[cell]);
+
+                rowDiv.appendChild(cellDiv);
+
+            });
+
+            mapContainer.appendChild(rowDiv);
+
+        });
+
+    });
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require('express');
 
 const app = express();
 
+const bookings = JSON.parse(
+    fs.readFileSync("data/bookings.json", "utf8")
+);
+
 const mapData = fs.readFileSync('data/map.ascii', 'utf8').trim().split('\n');
 
-console.log(mapData);
+console.log(bookings);
 
 const PORT = 3000;
 
@@ -17,6 +21,10 @@ app.use(express.json());
 // test route
 app.get('/api/map', function(req, res){
     res.json(mapData);
+});
+
+app.get("/api/bookings", function(req, res){
+    res.json(bookings);
 });
 
 app.listen(PORT, function(){
